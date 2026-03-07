b8c1a87e97b18654e7bc99a2112fa7ceeed28fe2
amber-hash <87498276+amber-hash@users.noreply.github.com>
2026-03-07 14:56:27 -0500

    Delete models directory

--- models/MarketDate.cpp
+++
@@ -1,5 +0,0 @@
-#include "MarketDate.h"
-
-MarketDate::MarketDate(int id, const QDate& date, int maxFood, int maxArtisan)
-    : m_id(id), m_date(date), m_maxFood(maxFood), m_maxArtisan(maxArtisan),
-      m_bookedFood(0), m_bookedArtisan(0) {}
